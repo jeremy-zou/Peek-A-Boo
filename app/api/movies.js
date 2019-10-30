d08cca158b816455ba2343f8e120871c75fa9d8d
jeremy-zou <jeremy-zou@users.noreply.github.com>
2019-10-30 02:27:31 -0400

    Folder cleanup

---
+++ app/api/movies.js
@@ -0,0 +1,533 @@
+export default 
+[{"name": "10 Cloverfield Lane (2016)"}, {"name": 
+"28 Days Later (2002)"}, {"name": 
+"28 Weeks Later (2007)"}, {"name": 
+"30 Days of Night (2007)"}, {"name": 
+"31 (2016)"}, {"name": 
+"47 Meters Down (2017)"}, {"name": 
+"1408 (2007)"}, {"name": 
+"1922 (2017)"}, {"name": 
+"A Cure for Wellness (2016)"}, {"name": 
+"A Ghost Story (2017)"}, {"name": 
+"A Girl Walks Home Alone at Night (2014)"}, {"name": 
+"A Nightmare on Elm Street (1984)"}, {"name": 
+"A Nightmare on Elm Street (2010)"}, {"name": 
+"A Nightmare on Elm Street 2 Freddys Revenge (1985)"}, {"name": 
+"A Nightmare on Elm Street 3 Dream Warriors (1987)"}, {"name": 
+"A Quiet Place (2018)"}, {"name": 
+"A Tale of Two Sisters (2003)"}, {"name": 
+"Absentia (2011)"}, {"name": 
+"Alien (1979)"}, {"name": 
+"Alien Covenant (2017)"}, {"name": 
+"Alien 3 (1992)"}, {"name": 
+"Alien Resurrection (1997)"}, {"name": 
+"Aliens (1986)"}, {"name": 
+"Alone (2007)"}, {"name": 
+"Amityville The Awakening (2017)"}, {"name": 
+"Amnesiac (2015)"}, {"name": 
+"An American Werewolf in London (1981)"}, {"name": 
+"An American Werewolf in Paris (1997)"}, {"name": 
+"Anaconda (1997)"}, {"name": 
+"Anna (2013)"}, {"name": 
+"Annabelle (2014)"}, {"name": 
+"Annabelle Comes Home (2019)"}, {"name": 
+"Annabelle Creation (2017)"}, {"name": 
+"Annihilation (2018)"}, {"name": 
+"Apollo 18 (2011)"}, {"name": 
+"Apostle (2018)"}, {"name": 
+"Army of Darkness (1992)"}, {"name": 
+"Arrival (2016)"}, {"name": 
+"Attack the Block (2011)"}, {"name": 
+"Audition (1999)"}, {"name": 
+"Backcountry (2014)"}, {"name": 
+"Backtrack (2015)"}, {"name": 
+"Banshee Chapter (2013)"}, {"name": 
+"Before I Wake (2016)"}, {"name": 
+"Berberian Sound Studio (2012)"}, {"name": 
+"Berlin Syndrome (2017)"}, {"name": 
+"Better Watch Out (2016)"}, {"name": 
+"Bird Box (2018)"}, {"name": 
+"Black Christmas (1974)"}, {"name": 
+"Black Swan (2010)"}, {"name": 
+"Blade (1998)"}, {"name": 
+"Blair Witch (2016)"}, {"name": 
+"Bone Tomahawk (2015)"}, {"name": 
+"Bride of Chucky (1998)"}, {"name": 
+"Cabin Fever (2002)"}, {"name": 
+"Cam (2018)"}, {"name": 
+"Candyman (1992)"}, {"name": 
+"Cape Fear (1991)"}, {"name": 
+"Cargo (2017)"}, {"name": 
+"Carrie (1976)"}, {"name": 
+"Carrie (2013)"}, {"name": 
+"Case 39 (2009)"}, {"name": 
+"Cat People (1942)"}, {"name": 
+"Cell (2016)"}, {"name": 
+"Chernobyl Diaries (2012)"}, {"name": 
+"Children of the Corn (1984)"}, {"name": 
+"Childs Play (1988)"}, {"name": 
+"Childs Play (2019)"}, {"name": 
+"Cloverfield (2008)"}, {"name": 
+"Clown (2014)"}, {"name": 
+"Cooties (2014)"}, {"name": 
+"Creep (2014)"}, {"name": 
+"Creep 2 (2017)"}, {"name": 
+"Crimson Peak (2015)"}, {"name": 
+"Cube (1997)"}, {"name": 
+"Cujo (1983)"}, {"name": 
+"Cult of Chucky (2017)"}, {"name": 
+"Cyberbully (2015)"}, {"name": 
+"Dark Shadows (2012)"}, {"name": 
+"Dark Skies (2013)"}, {"name": 
+"Dark Was The Night (2014)"}, {"name": 
+"Dark Water (2005)"}, {"name": 
+"Dawn of the Dead (2004)"}, {"name": 
+"Day of the Dead (1985)"}, {"name": 
+"Dead Alive (1992)"}, {"name": 
+"Dead Calm (1989)"}, {"name": 
+"Dead End (2003)"}, {"name": 
+"Dead Silence (2007)"}, {"name": 
+"Death Note (2017)"}, {"name": 
+"Deathgasm (2015)"}, {"name": 
+"Deep Blue Sea (1999)"}, {"name": 
+"Deep Red (1975)"}, {"name": 
+"Deliver Us From Evil (2014)"}, {"name": 
+"Dementia (2015)"}, {"name": 
+"Demonic (2015)"}, {"name": 
+"Devil (2010)"}, {"name": 
+"Devils Due (2014)"}, {"name": 
+"Devils Pass (2013)"}, {"name": 
+"Dismissed (2017)"}, {"name": 
+"Disturbia (2007)"}, {"name": 
+"Dog Soldiers (2002)"}, {"name": 
+"Donnie Darko (2001)"}, {"name": 
+"Dont Be Afraid of the Dark (2010)"}, {"name": 
+"Dont Breathe (2016)"}, {"name": 
+"Dont Knock Twice (2016)"}, {"name": 
+"Doom (2005)"}, {"name": 
+"Dracula Untold (2014)"}, {"name": 
+"Drag Me To Hell (2009)"}, {"name": 
+"Dreamcatcher (2003)"}, {"name": 
+"Eden Lake (2008)"}, {"name": 
+"Eli (2019)"}, {"name": 
+"Elle (2016)"}, {"name": 
+"Emelie (2015)"}, {"name": 
+"Escape Room (2019)"}, {"name": 
+"Europa Report (2013)"}, {"name": 
+"Event Horizon (1997)"}, {"name": 
+"Evil Dead (2013)"}, {"name": 
+"Evil Dead II (1987)"}, {"name": 
+"Exorcist The Beginning (2004)"}, {"name": 
+"Extinction (2015)"}, {"name": 
+"Extraterrestrial (2014)"}, {"name": 
+"Fear Clinic (2014)"}, {"name": 
+"Final Destination (2000)"}, {"name": 
+"Final Destination 2 (2003)"}, {"name": 
+"Final Destination 3 (2006)"}, {"name": 
+"Final Destination 5 (2011)"}, {"name": 
+"Flatliners (1990)"}, {"name": 
+"Flatliners (2017)"}, {"name": 
+"Frailty (2001)"}, {"name": 
+"Freddy vs Jason (2003)"}, {"name": 
+"Friday the 13th (1980)"}, {"name": 
+"Friday the 13th (2009)"}, {"name": 
+"Friend Request (2016)"}, {"name": 
+"Fright Night (2011)"}, {"name": 
+"Funny Games (2007)"}, {"name": 
+"Geralds Game (2017)"}, {"name": 
+"Get Out (2017)"}, {"name": 
+"Ghost Ship (2002)"}, {"name": 
+"Ghost Stories (2017)"}, {"name": 
+"Ghostland (2018)"}, {"name": 
+"Goodnight Mommy (2014)"}, {"name": 
+"Goosebumps (2015)"}, {"name": 
+"Gothika (2003)"}, {"name": 
+"Grave Encounters (2011)"}, {"name": 
+"Green Room (2015)"}, {"name": 
+"Gremlins (1984)"}, {"name": 
+"Halloween (1978)"}, {"name": 
+"Halloween (2018)"}, {"name": 
+"Halloween H20 20 Years Later (1998)"}, {"name": 
+"Halloween II (1981)"}, {"name": 
+"Halloween II (2009)"}, {"name": 
+"Hannibal (2001)"}, {"name": 
+"Hannibal Rising (2007)"}, {"name": 
+"Happy Death Day 2U (2019)"}, {"name": 
+"Happy Death Day (2017)"}, {"name": 
+"Hellboy (2004)"}, {"name": 
+"Hellraiser (1987)"}, {"name": 
+"Here Alone (2016)"}, {"name": 
+"Hereditary (2018)"}, {"name": 
+"Hidden (2015)"}, {"name": 
+"Hide and Seek (2005)"}, {"name": 
+"High Tension (2003)"}, {"name": 
+"Holidays (2016)"}, {"name": 
+"Hollow Man (2000)"}, {"name": 
+"Honeymoon (2014)"}, {"name": 
+"Horns (2013)"}, {"name": 
+"Hostel (2005)"}, {"name": 
+"Hounds of Love (2016)"}, {"name": 
+"Hour of the Wolf (1968)"}, {"name": 
+"House (1986)"}, {"name": 
+"House of 1000 Corpses (2003)"}, {"name": 
+"House of Wax (2005)"}, {"name": 
+"House on Haunted Hill (1999)"}, {"name": 
+"Hush (2016)"}, {"name": 
+"I Am Legend (2007)"}, {"name": 
+"I Am Not a Serial Killer (2016)"}, {"name": 
+"I Am the Pretty Thing That Lives in the House (2016)"}, {"name": 
+"I Know What You Did Last Summer (1997)"}, {"name": 
+"Identity (2003)"}, {"name": 
+"Idle Hands (1999)"}, {"name": 
+"In The Mouth Of Madness (1994)"}, {"name": 
+"In the Tall Grass (2019)"}, {"name": 
+"Incarnate (2016)"}, {"name": 
+"Inland Empire (2006)"}, {"name": 
+"Inside (2007)"}, {"name": 
+"Insidious (2010)"}, {"name": 
+"Insidious Chapter 2 (2013)"}, {"name": 
+"Insidious Chapter 3 (2015)"}, {"name": 
+"Insidious The Last Key (2018)"}, {"name": 
+"Interview With the Vampire (1994)"}, {"name": 
+"Intruders (2015)"}, {"name": 
+"Invasion of the Body Snatchers (1978)"}, {"name": 
+"It (2017)"}, {"name": 
+"It Comes at Night (2017)"}, {"name": 
+"It Follows (2014)"}, {"name": 
+"Jacobs Ladder (1990)"}, {"name": 
+"Jaws (1975)"}, {"name": 
+"Jeepers Creepers (2001)"}, {"name": 
+"Jennifers Body (2009)"}, {"name": 
+"Jessabelle (2014)"}, {"name": 
+"Jigsaw (2017)"}, {"name": 
+"Joy Ride (2001)"}, {"name": 
+"Julias Eyes (2010)"}, {"name": 
+"Jurassic Park (1993)"}, {"name": 
+"Jurassic World (2015)"}, {"name": 
+"Jurassic World Fallen Kingdom (2018)"}, {"name": 
+"Kill List (2011)"}, {"name": 
+"Knock Knock (2015)"}, {"name": 
+"Krampus (2015)"}, {"name": 
+"Lake Mungo (2008)"}, {"name": 
+"Lake Placid (1999)"}, {"name": 
+"Last Shift (2014)"}, {"name": 
+"Leatherface (2017)"}, {"name": 
+"Let Me In (2010)"}, {"name": 
+"Let The Right One In (2008)"}, {"name": 
+"Let Us Prey (2014)"}, {"name": 
+"Life (2017)"}, {"name": 
+"Lights Out (2016)"}, {"name": 
+"Little Evil (2017)"}, {"name": 
+"Ma (2019)"}, {"name": 
+"Maggie (2015)"}, {"name": 
+"Malevolent (2018)"}, {"name": 
+"Malice (1993)"}, {"name": 
+"Mama (2013)"}, {"name": 
+"Mandy (2018)"}, {"name": 
+"Marrowbone (2017)"}, {"name": 
+"Martyrs (2008)"}, {"name": 
+"May (2002)"}, {"name": 
+"Mayhem (2017)"}, {"name": 
+"Megan Is Missing (2011)"}, {"name": 
+"Midsommar (2019)"}, {"name": 
+"Mirrors (2008)"}, {"name": 
+"Misery (1990)"}, {"name": 
+"Mom and Dad (2017)"}, {"name": 
+"Morgan (2016)"}, {"name": 
+"Mother (2017)"}, {"name": 
+"Mulholland Drive (2001)"}, {"name": 
+"My Bloody Valentine (1981)"}, {"name": 
+"My Friend Dahmer (2017)"}, {"name": 
+"Nerve (2016)"}, {"name": 
+"Nocturnal Animals (2016)"}, {"name": 
+"Noroi The Curse (2005)"}, {"name": 
+"Oculus (2013)"}, {"name": 
+"Odd Thomas (2013)"}, {"name": 
+"One Hour Photo (2002)"}, {"name": 
+"One Missed Call (2008)"}, {"name": 
+"Only Lovers Left Alive (2013)"}, {"name": 
+"Orphan (2009)"}, {"name": 
+"Ouija (2014)"}, {"name": 
+"Overlord (2018)"}, {"name": 
+"P2 (2007)"}, {"name": 
+"Pandorum (2009)"}, {"name": 
+"Pans Labyrinth (2006)"}, {"name": 
+"Paranormal Activity (2007)"}, {"name": 
+"Paranormal Activity 2 (2010)"}, {"name": 
+"Paranormal Activity 3 (2011)"}, {"name": 
+"Paranormal Activity 4 (2012)"}, {"name": 
+"Paranormal Activity The Ghost Dimension (2015)"}, {"name": 
+"Patient Zero (2018)"}, {"name": 
+"Pay The Ghost (2015)"}, {"name": 
+"Personal Shopper (2016)"}, {"name": 
+"Pet (2016)"}, {"name": 
+"Pet Sematary (1989)"}, {"name": 
+"Pet Sematary (2019)"}, {"name": 
+"Planet Terror (2007)"}, {"name": 
+"Poltergeist (1982)"}, {"name": 
+"Poltergeist (2015)"}, {"name": 
+"Pontypool (2008)"}, {"name": 
+"Predator (1987)"}, {"name": 
+"Prevenge (2016)"}, {"name": 
+"Pride and Prejudice and Zombies (2016)"}, {"name": 
+"Prince of Darkness (1987)"}, {"name": 
+"Prometheus (2012)"}, {"name": 
+"Psycho (1960)"}, {"name": 
+"Quarantine (2008)"}, {"name": 
+"Ravenous (1999)"}, {"name": 
+"Raw (2016)"}, {"name": 
+"Red Dragon (2002)"}, {"name": 
+"Regression (2015)"}, {"name": 
+"Repulsion (1965)"}, {"name": 
+"Resident Evil (2002)"}, {"name": 
+"Resident Evil The Final Chapter (2016)"}, {"name": 
+"Resolution (2012)"}, {"name": 
+"Restoration (2016)"}, {"name": 
+"Ring (1998)"}, {"name": 
+"Rings (2017)"}, {"name": 
+"Rosemarys Baby (1968)"}, {"name": 
+"Saw (2004)"}, {"name": 
+"Saw II (2005)"}, {"name": 
+"Saw III (2006)"}, {"name": 
+"Saw IV (2007)"}, {"name": 
+"Saw V (2008)"}, {"name": 
+"Saw VI (2009)"}, {"name": 
+"Scary Movie (2000)"}, {"name": 
+"Scary Movie 2 (2001)"}, {"name": 
+"Scream (1996)"}, {"name": 
+"Scream 2 (1997)"}, {"name": 
+"Scream 3 (2000)"}, {"name": 
+"Scream 4 (2011)"}, {"name": 
+"Searching (2018)"}, {"name": 
+"Secret Window (2004)"}, {"name": 
+"Session 9 (2001)"}, {"name": 
+"Seven (1995)"}, {"name": 
+"Shaun of the Dead (2004)"}, {"name": 
+"Shutter (2004)"}, {"name": 
+"Shutter Island (2010)"}, {"name": 
+"Signs (2002)"}, {"name": 
+"Silent Hill (2006)"}, {"name": 
+"Silent Hill Revelation (2012)"}, {"name": 
+"Silver Bullet (1985)"}, {"name": 
+"Sinister (2012)"}, {"name": 
+"Sinister 2 (2015)"}, {"name": 
+"Sleepaway Camp (1983)"}, {"name": 
+"Slender Man (2018)"}, {"name": 
+"Slither (2006)"}, {"name": 
+"Slumber (2017)"}, {"name": 
+"Species (1995)"}, {"name": 
+"Sphere (1998)"}, {"name": 
+"Splinter (2008)"}, {"name": 
+"Split (2016)"}, {"name": 
+"Spring (2014)"}, {"name": 
+"Stake Land (2010)"}, {"name": 
+"Starry Eyes (2014)"}, {"name": 
+"Stephanie (2017)"}, {"name": 
+"Stir of Echoes (1999)"}, {"name": 
+"Summer of 84 (2018)"}, {"name": 
+"Sunshine (2007)"}, {"name": 
+"Super 8 (2011)"}, {"name": 
+"Suspiria (1977)"}, {"name": 
+"Suspiria (2018)"}, {"name": 
+"Tales of Halloween (2015)"}, {"name": 
+"Terrifier (2017)"}, {"name": 
+"Texas Chainsaw 3D (2013)"}, {"name": 
+"The 9th Life of Louis Drax (2016)"}, {"name": 
+"The Abandoned (2015)"}, {"name": 
+"The ABCs of Death (2012)"}, {"name": 
+"The Abyss (1989)"}, {"name": 
+"The Amityville Horror (1979)"}, {"name": 
+"The Amityville Horror (2005)"}, {"name": 
+"The Atticus Institute (2015)"}, {"name": 
+"The Babadook (2014)"}, {"name": 
+"The Babysitter (2017)"}, {"name": 
+"The Bay (2012)"}, {"name": 
+"The Belko Experiment (2016)"}, {"name": 
+"The Blackcoats Daughter (2015)"}, {"name": 
+"The Blair Witch Project (1999)"}, {"name": 
+"The Borderlands (Final Prayer) (2013)"}, {"name": 
+"The Boy (2015)"}, {"name": 
+"The Boy (2016)"}, {"name": 
+"The Burning (1981)"}, {"name": 
+"The Butterfly Effect (2004)"}, {"name": 
+"The Bye Bye Man (2017)"}, {"name": 
+"The Cabin In The Woods (2012)"}, {"name": 
+"The Call (2013)"}, {"name": 
+"The Canal (2014)"}, {"name": 
+"The Changeling (1980)"}, {"name": 
+"The Cloverfield Paradox (2018)"}, {"name": 
+"The Collector (2009)"}, {"name": 
+"The Conjuring (2013)"}, {"name": 
+"The Conjuring 2 (2016)"}, {"name": 
+"The Conspiracy (2012)"}, {"name": 
+"The Conversation (1974)"}, {"name": 
+"The Craft (1996)"}, {"name": 
+"The Crazies (2010)"}, {"name": 
+"The Crucifixion (2017)"}, {"name": 
+"The Cured (2017)"}, {"name": 
+"The Curse of La Llorona (2019)"}, {"name": 
+"The Dead Dont Die (2019)"}, {"name": 
+"The Den (2013)"}, {"name": 
+"The Descent (2005)"}, {"name": 
+"The Devil Inside (2012)"}, {"name": 
+"The Devils Backbone (2001)"}, {"name": 
+"The Devils Candy (2015)"}, {"name": 
+"The Devils Rejects (2005)"}, {"name": 
+"The Disappointments Room (2016)"}, {"name": 
+"The Endless (2017)"}, {"name": 
+"The Evil Dead (1981)"}, {"name": 
+"The Exorcism of Emily Rose (2005)"}, {"name": 
+"The Exorcist (1973)"}, {"name": 
+"The Exorcist III (1990)"}, {"name": 
+"The Eye (2002)"}, {"name": 
+"The Eyes of My Mother (2016)"}, {"name": 
+"The Faculty (1998)"}, {"name": 
+"The Final Destination (2009)"}, {"name": 
+"The Final Girls (2015)"}, {"name": 
+"The First Purge (2018)"}, {"name": 
+"The Fly (1986)"}, {"name": 
+"The Fog (1980)"}, {"name": 
+"The Forest (2016)"}, {"name": 
+"The Forgotten (2004)"}, {"name": 
+"The Gallows (2015)"}, {"name": 
+"The Gift (2015)"}, {"name": 
+"The Girl with All the Gifts (2016)"}, {"name": 
+"The Good Neighbor (2016)"}, {"name": 
+"The Green Inferno (2013)"}, {"name": 
+"The Grudge (2004)"}, {"name": 
+"The Guest (2014)"}, {"name": 
+"The Hallow (2015)"}, {"name": 
+"The Happening (2008)"}, {"name": 
+"The Harvest (2013)"}, {"name": 
+"The Haunting (1963)"}, {"name": 
+"The Haunting in Connecticut (2009)"}, {"name": 
+"The Haunting in Connecticut 2 Ghosts of Georgia (2013)"}, {"name": 
+"The Hills Have Eyes (2006)"}, {"name": 
+"The Hole (2001)"}, {"name": 
+"The House of the Devil (2009)"}, {"name": 
+"The House on Pine Street (2015)"}, {"name": 
+"The House with a Clock in Its Walls (2018)"}, {"name": 
+"The Howling (1981)"}, {"name": 
+"The Human Centipede (First Sequence) (2009)"}, {"name": 
+"The Innkeepers (2011)"}, {"name": 
+"The Innocents (1961)"}, {"name": 
+"The Invitation (2015)"}, {"name": 
+"The Jacket (2005)"}, {"name": 
+"The Killing of a Sacred Deer (2017)"}, {"name": 
+"The Last Exorcism (2010)"}, {"name": 
+"The Last House on the Left (2009)"}, {"name": 
+"The Lazarus Effect (2015)"}, {"name": 
+"The Limehouse Golem (2016)"}, {"name": 
+"The Little Stranger (2018)"}, {"name": 
+"The Love Witch (2016)"}, {"name": 
+"The Meg (2018)"}, {"name": 
+"The Messengers (2007)"}, {"name": 
+"The Midnight Meat Train (2008)"}, {"name": 
+"The Mist (2007)"}, {"name": 
+"The Monster (2016)"}, {"name": 
+"The Mothman Prophecies (2002)"}, {"name": 
+"The Mummy (2017)"}, {"name": 
+"The Neighbor (2016)"}, {"name": 
+"The Neon Demon (2016)"}, {"name": 
+"The New Daughter (2009)"}, {"name": 
+"The Ninth Gate (1999)"}, {"name": 
+"The Number 23 (2007)"}, {"name": 
+"The Nun (2018)"}, {"name": 
+"The Omen (1976)"}, {"name": 
+"The Ones Below (2015)"}, {"name": 
+"The Open House (2018)"}, {"name": 
+"The Orphanage (2007)"}, {"name": 
+"The Other Side of the Door (2016)"}, {"name": 
+"The Others (2001)"}, {"name": 
+"The Pact (2012)"}, {"name": 
+"The People Under the Stairs (1991)"}, {"name": 
+"The Perfection (2019)"}, {"name": 
+"The Possession (2012)"}, {"name": 
+"The Possession of Michael King (2014)"}, {"name": 
+"The Purge (2013)"}, {"name": 
+"The Purge Anarchy (2014)"}, {"name": 
+"The Purge Election Year (2016)"}, {"name": 
+"The Quiet Ones (2014)"}, {"name": 
+"The Return of the Living Dead (1985)"}, {"name": 
+"The Ring (2002)"}, {"name": 
+"The Ring Two (2005)"}, {"name": 
+"The Rite (2011)"}, {"name": 
+"The Ritual (2017)"}, {"name": 
+"The Sacrament (2013)"}, {"name": 
+"The Shallows (2016)"}, {"name": 
+"The Shape of Water (2017)"}, {"name": 
+"The Shining (1980)"}, {"name": 
+"The Silence of the Lambs (1991)"}, {"name": 
+"The Sixth Sense (1999)"}, {"name": 
+"The Skeleton Key (2005)"}, {"name": 
+"The Snowman (2017)"}, {"name": 
+"The Strangers (2008)"}, {"name": 
+"The Strangers Prey at Night (2018)"}, {"name": 
+"The Taking of Deborah Logan (2014)"}, {"name": 
+"The Texas Chain Saw Massacre (1974)"}, {"name": 
+"The Texas Chainsaw Massacre (2003)"}, {"name": 
+"The Texas Chainsaw Massacre The Beginning (2006)"}, {"name": 
+"The Thing (1982)"}, {"name": 
+"The Thing (2011)"}, {"name": 
+"The Town That Dreaded Sundown (2014)"}, {"name": 
+"The Transfiguration (2016)"}, {"name": 
+"The Tunnel (2011)"}, {"name": 
+"The Unborn (2009)"}, {"name": 
+"The Uninvited (2009)"}, {"name": 
+"The Veil (2016)"}, {"name": 
+"The Village (2004)"}, {"name": 
+"The Visit (2015)"}, {"name": 
+"The Voices (2014)"}, {"name": 
+"The Void (2016)"}, {"name": 
+"The Wailing (2016)"}, {"name": 
+"The Ward (2010)"}, {"name": 
+"The Wicker Man (1973)"}, {"name": 
+"The Witch (2015)"}, {"name": 
+"The Wolfman (2010)"}, {"name": 
+"The Woman in Black (2012)"}, {"name": 
+"The Woman in Black 2 Angel of Death (2014)"}, {"name": 
+"Them (2006)"}, {"name": 
+"Thesis (1996)"}, {"name": 
+"They Look Like People (2015)"}, {"name": 
+"Thirteen Ghosts (2001)"}, {"name": 
+"Timecrimes (2007)"}, {"name": 
+"Tragedy Girls (2017)"}, {"name": 
+"Train to Busan (2016)"}, {"name": 
+"Trash Fire (2016)"}, {"name": 
+"Tremors (1990)"}, {"name": 
+"Triangle (2009)"}, {"name": 
+"Trick r Treat (2007)"}, {"name": 
+"Truth or Dare (2018)"}, {"name": 
+"Tucker and Dale vs Evil (2010)"}, {"name": 
+"Tusk (2014)"}, {"name": 
+"Under the Shadow (2016)"}, {"name": 
+"Unfriended (2014)"}, {"name": 
+"Unfriended Dark Web (2018)"}, {"name": 
+"Unsane (2018)"}, {"name": 
+"Urban Legend (1998)"}, {"name": 
+"Us (2019)"}, {"name": 
+"Uzumaki (2000)"}, {"name": 
+"Vacancy (2007)"}, {"name": 
+"Velvet Buzzsaw (2019)"}, {"name": 
+"Veronica (2017)"}, {"name": 
+"VHS (2012)"}, {"name": 
+"VHS2 (2013)"}, {"name": 
+"Victor Frankenstein (2015)"}, {"name": 
+"Viral (2016)"}, {"name": 
+"Visions (2015)"}, {"name": 
+"Wait Until Dark (1967)"}, {"name": 
+"We Are Still Here (2015)"}, {"name": 
+"Wes Cravens New Nightmare (1994)"}, {"name": 
+"What Lies Beneath (2000)"}, {"name": 
+"When a Stranger Calls (1979)"}, {"name": 
+"Wildling (2018)"}, {"name": 
+"Winchester (2018)"}, {"name": 
+"Wind Chill (2007)"}, {"name": 
+"Wish Upon (2017)"}, {"name": 
+"Wolf Creek (2005)"}, {"name": 
+"World War Z (2013)"}, {"name": 
+"Would You Rather (2012)"}, {"name": 
+"Youre Next (2011)"}, {"name": 
+"Zodiac (2007)"}, {"name": 
+"Zombieland (2009)"}
+]
